3903bb4e29edb27f3d5effa0b7f05ba8dc8abcc4
Andrew Top <aabtop@google.com>
2016-09-02 09:46:38 -0700

    Update win_tools.bat to reflect changes in external URLs.
    Also remove unnecessary download and setup of SVN.
    
    Change-Id: I5573d218aebc1be618510a1af75ed2794e259364

--- bootstrap/win/win_tools.bat
+++ bootstrap/win/win_tools.bat
@@ -9,7 +9,7 @@
 :: Sadly, we can't use SETLOCAL here otherwise it ERRORLEVEL is not correctly
 :: returned.
 
-set WIN_TOOLS_ROOT_URL=https://src.chromium.org/svn/trunk/tools
+set WIN_TOOLS_ROOT_URL=https://storage.googleapis.com/chrome-infra/
 :: It used to be %~dp0 but ADODB.Stream may fail to write to this directory if
 :: the directory DACL is set to elevated integrity level.
 set ZIP_DIR=%TEMP%
@@ -37,8 +37,8 @@ echo Installing python 2.7.6...
 :: Cleanup python directory if it was existing.
 if exist "%WIN_TOOLS_ROOT_DIR%\python276_bin\." rd /q /s "%WIN_TOOLS_ROOT_DIR%\python276_bin"
 if exist "%ZIP_DIR%\python276.zip" del "%ZIP_DIR%\python276.zip"
-echo Fetching from %WIN_TOOLS_ROOT_URL%/third_party/python276_bin.zip
-cscript //nologo //e:jscript "%~dp0get_file.js" %WIN_TOOLS_ROOT_URL%/third_party/python276_bin.zip "%ZIP_DIR%\python276_bin.zip"
+echo Fetching from %WIN_TOOLS_ROOT_URL%python276_bin.zip
+cscript //nologo //e:jscript "%~dp0get_file.js" %WIN_TOOLS_ROOT_URL%python276_bin.zip "%ZIP_DIR%\python276_bin.zip"
 if errorlevel 1 goto :PYTHON_FAIL
 :: Will create python276_bin\...
 cscript //nologo //e:jscript "%~dp0unzip.js" "%ZIP_DIR%\python276_bin.zip" "%WIN_TOOLS_ROOT_DIR%"
@@ -52,7 +52,7 @@ goto :GIT_CHECK
 
 :PYTHON_FAIL
 echo ... Failed to checkout python automatically.
-echo You should get the "prebaked" version at %WIN_TOOLS_ROOT_URL%/third_party/
+echo You should get the "prebaked" version at %WIN_TOOLS_ROOT_URL%
 set ERRORLEVEL=1
 goto :END
 
@@ -85,7 +85,7 @@ if exist "%WIN_TOOLS_ROOT_DIR%\%GIT_BIN_DIR%\cmd\git.cmd" (
   rem batch script to make sure it points to the desired version.
   find "%GIT_BIN_DIR%" "%WIN_TOOLS_ROOT_DIR%\git.bat" 2>nul 1>nul
   if errorlevel 1 goto :GIT_COPY_BATCH_FILES
-  goto :SVN_CHECK
+  goto :END
 )
 goto :GIT_INSTALL
 
@@ -117,7 +117,7 @@ call copy /y "%WIN_TOOLS_ROOT_DIR%\%GIT_BIN_DIR%\ssh-keygen.bat" "%WIN_TOOLS_ROO
 :: Ensure autocrlf and filemode are set correctly.
 call "%WIN_TOOLS_ROOT_DIR%\%GIT_BIN_DIR%\cmd\git.cmd" config --system core.autocrlf false
 call "%WIN_TOOLS_ROOT_DIR%\%GIT_BIN_DIR%\cmd\git.cmd" config --system core.filemode false
-goto :SVN_CHECK
+goto :END
 
 
 :GIT_FAIL
@@ -125,44 +125,6 @@ echo ... Failed to checkout git automatically.
 echo You should get the "prebaked" version used at %GIT_ZIP_URL%
 set ERRORLEVEL=1
 goto :END
-
-
-:SVN_CHECK
-:: If the batch file exists, skip the svn check.
-if exist "%WIN_TOOLS_ROOT_DIR%\svn.bat" goto :END
-if "%WIN_TOOLS_FORCE%" == "1" goto :SVN_INSTALL
-call svn --version 2>nul 1>nul
-if errorlevel 1 goto :SVN_INSTALL
-goto :END
-
-
-:SVN_INSTALL
-echo Installing subversion ...
-:: svn is not accessible; check it out and create 'proxy' files.
-if exist "%ZIP_DIR%\svn.zip" del "%ZIP_DIR%\svn.zip"
-echo Fetching from %WIN_TOOLS_ROOT_URL%/third_party/svn_bin.zip
-cscript //nologo //e:jscript "%~dp0get_file.js" %WIN_TOOLS_ROOT_URL%/third_party/svn_bin.zip "%ZIP_DIR%\svn.zip"
-if errorlevel 1 goto :SVN_FAIL
-:: Cleanup svn directory if it was existing.
-if exist "%WIN_TOOLS_ROOT_DIR%\svn\." rd /q /s "%WIN_TOOLS_ROOT_DIR%\svn"
-if exist "%WIN_TOOLS_ROOT_DIR%\svn_bin\." rd /q /s "%WIN_TOOLS_ROOT_DIR%\svn_bin"
-:: Will create svn_bin\...
-cscript //nologo //e:jscript "%~dp0unzip.js" "%ZIP_DIR%\svn.zip" "%WIN_TOOLS_ROOT_DIR%"
-if errorlevel 1 goto :SVN_FAIL
-if not exist "%WIN_TOOLS_ROOT_DIR%\svn_bin\." goto :SVN_FAIL
-del "%ZIP_DIR%\svn.zip"
-:: Create the batch file.
-call copy /y "%~dp0svn.new.bat" "%WIN_TOOLS_ROOT_DIR%\svn.bat" 1>nul
-call copy /y "%~dp0svnversion.new.bat" "%WIN_TOOLS_ROOT_DIR%\svnversion.bat" 1>nul
-goto :END
-
-
-:SVN_FAIL
-echo ... Failed to checkout svn automatically.
-echo You should get the "prebaked" version at %WIN_TOOLS_ROOT_URL%/third_party/
-set ERRORLEVEL=1
-goto :END
-
 
 :returncode
 set WIN_TOOLS_ROOT_URL=
